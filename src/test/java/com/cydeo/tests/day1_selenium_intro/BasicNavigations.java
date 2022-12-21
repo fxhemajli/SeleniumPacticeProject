534bbc2d67c6fabcee6ba70a4c346fe54bb2971f
fxhemajli <109180076+fxhemajli@users.noreply.github.com>
2022-12-21 22:31:59 +0100

    added some navgations

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -10,6 +10,8 @@ public class BasicNavigations {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.tesla.com");
+        String currentTitle1 = driver.getTitle();
+        System.out.println(currentTitle1);
 
         //stop code execution for 3 seconds
         Thread.sleep(3000);
@@ -17,11 +19,26 @@ public class BasicNavigations {
         // use selenium to navigate back
         driver.navigate().back();
 
-        //stop code execution for 3 seconds
-        Thread.sleep(3000);
+
 
         // use selenium to navigate forward
         driver.navigate().forward();
+
+
+
+        // use selenium to navigate refresh
+        driver.navigate().refresh();
+
+
+
+        //use navigate.to();
+        driver.navigate().to("https://www.google.com");
+
+        //get the title of the page
+        currentTitle1 = driver.getTitle();
+        System.out.println("currentTitle1 = " + currentTitle1);
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentURL);
 
 
     }
